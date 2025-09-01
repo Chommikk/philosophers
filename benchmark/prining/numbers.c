7cd53ac097f8da88a0167c44dd4d7e2f0925f9ea
Michal Choma <mchoma@c3r8p6.42vienna.com>
2025-09-01 15:43:29 +0200

    now will be doing testing

--- benchmark/prining/numbers.c
+++ benchmark/prining/numbers.c
@@ -6,27 +6,33 @@
 void	print_died(int count)
 {
 	char				str[100];
-	/*
-	str[8] = 'r';
-	str[9] = 'l';
-	str[10] = 'd';
-	str[11] = '\n';
 	unsigned long long	i;
-	memmove(&i, str + 8,8);
-	printf("sizeof() %lu\n", sizeof(unsigned long long));
-	*/
-	*((unsigned long long*)str) = 0x6f77206f6c6c6568;
-	*((unsigned long long*)(str + 8)) = 0xa646c72;
+	unsigned long long	j;
+
+	// has taken a fork\n
+	memmove(&i, " is slee", 8);
+	printf("%llx\n", i);
+	*((unsigned long long*)str) = i;
+	bzero(&j, 8);
+	memmove(&j, "ping\n", 5);
+	printf("%llx\n", j);
+	*((unsigned long long*)(str + 8)) = j;
+	// *(unsigned long long*)(str) = 0x6974616520736920;
+	// *(unsigned long long*)(str + 8) = 0xa676e;
+	write(1, str, 13);
 }
 int main()
 {
 	int i = 0;
 	clock_t start = clock();
+	print_died(i);
+	/*
 	while(i < 10000000)
 	{
 		print_died(i);
 		i ++;
 	}
+	*/
 	clock_t end = clock();
 	double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
     fprintf(stderr, "Execution time: %f seconds\n", time_taken);
